3a45f69a1c936fab37690445ec2d888b0fedeb8c
datvt6 <datvt6@vng.com.vn>
2025-08-18 13:34:20 +0700

    feat: Implement aiming logic based on mode
    Added aim functions for head, neck, chest, and abdomen.
    Uses bounding boxes to determine aim point and zone.

--- src/aim.rs
+++ src/aim.rs
@@ -1,3 +1,7 @@
+use crate::{
+    config::{SCALE_ABDOMEN_Y, SCALE_CHEST_Y, SCALE_HEAD_Y, SCALE_NECK_Y},
+    model::{Bbox, Point2f},
+};
 use std::sync::{
     Arc,
     atomic::{AtomicU8, Ordering},
@@ -23,6 +27,80 @@ impl AimMode {
 
     pub fn set_mode(&self, mode: Mode) {
         self.0.store(mode.into(), Ordering::Release);
+    }
+
+    pub fn aim(&self, bboxes: &Vec<Bbox>) -> Option<(Point2f, f32)> {
+        match self.mode() {
+            Mode::Head => self.aim_head(bboxes),
+            Mode::Neck => self.aim_neck(bboxes),
+            Mode::Chest => self.aim_chest(bboxes),
+            Mode::Abdomen => self.aim_abdomen(bboxes),
+        }
+    }
+
+    pub fn aim_head(&self, bboxes: &Vec<Bbox>) -> Option<(Point2f, f32)> {
+        match bboxes.first() {
+            Some(bbox) => Some(if bbox.class() == 1 {
+                (bbox.cxcy(), (bbox.width() / 2.).max(bbox.height() / 2.))
+            } else {
+                (
+                    bbox.cxcy_scale(None, Some(SCALE_HEAD_Y)),
+                    (bbox.width() / 2.).max(bbox.height() / 2. * SCALE_HEAD_Y),
+                )
+            }),
+            _ => None,
+        }
+    }
+
+    pub fn aim_neck(&self, bboxes: &Vec<Bbox>) -> Option<(Point2f, f32)> {
+        match bboxes.first() {
+            Some(bbox) => Some(if bbox.class() == 1 {
+                (
+                    Point2f::new((bbox.xmax() - bbox.xmin()) / 2., bbox.ymax()),
+                    (bbox.width() / 2.).max(bbox.height() / 2.),
+                )
+            } else {
+                (
+                    bbox.cxcy_scale(None, Some(SCALE_NECK_Y)),
+                    (bbox.width() / 2.).max(bbox.height() / 2. * SCALE_NECK_Y),
+                )
+            }),
+            _ => None,
+        }
+    }
+
+    pub fn aim_chest(&self, bboxes: &Vec<Bbox>) -> Option<(Point2f, f32)> {
+        self.aim_low(bboxes, SCALE_CHEST_Y)
+    }
+
+    pub fn aim_abdomen(&self, bboxes: &Vec<Bbox>) -> Option<(Point2f, f32)> {
+        self.aim_low(bboxes, SCALE_ABDOMEN_Y)
+    }
+
+    #[inline(always)]
+    fn aim_low(&self, bboxes: &Vec<Bbox>, scale: f32) -> Option<(Point2f, f32)> {
+        match bboxes.first() {
+            Some(bbox) => Some(if bbox.class() == 1 {
+                let mut point = bbox.cxcy_scale(None, Some(scale / SCALE_HEAD_Y));
+                let mut min_zone =
+                    (bbox.width() / 2.).max(scale / SCALE_HEAD_Y * bbox.height() / 2.);
+                for i in 1..bboxes.len() {
+                    let bbox = bboxes[i];
+                    if bbox.class() == 0 {
+                        point = bbox.cxcy_scale(None, Some(scale));
+                        min_zone = (bbox.width() / 2.).max(scale * bbox.height() / 2.);
+                        break;
+                    }
+                }
+                (point, min_zone)
+            } else {
+                (
+                    bbox.cxcy_scale(None, Some(scale)),
+                    (bbox.width() / 2.).max(scale * bbox.height() / 2.),
+                )
+            }),
+            _ => None,
+        }
     }
 }
 

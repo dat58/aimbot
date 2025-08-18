9859ab7508ceb006cf62aa9f7f3b6b4fdfe1693d
datvt6 <datvt6@vng.com.vn>
2025-08-18 12:48:15 +0700

    feat: Refactor mouse control using serialport
    This commit replaces the makcu_rs crate with serialport for mouse control, adds baud rate switching and verification logic, and simplifies command execution.

--- src/mouse.rs
+++ src/mouse.rs
@@ -1,23 +1,77 @@
-use anyhow::Result;
-use makcu_rs as makcu;
+use anyhow::{Result, bail};
 use rand::prelude::*;
-use std::time::Duration;
+use serialport::{self, SerialPort};
+use std::{thread::sleep, time::Duration};
+
+const BAUD_CHANGE_COMMAND: [u8; 9] = [0xDE, 0xAD, 0x05, 0x00, 0xA5, 0x00, 0x09, 0x3D, 0x00];
+const VERIFY_COMMAND: &[u8] = b"km.version()\r\n";
+const DEFAULT_BAUD_RATE: u32 = 115_200;
+const ALLOWED_BAUD_RATE: [u32; 3] = [115_200, 2_000_000, 4_000_000];
 
 pub struct MouseVirtual {
-    device: makcu::Device,
+    serial: Box<dyn SerialPort>,
     random: ThreadRng,
 }
 
 impl MouseVirtual {
     pub fn new(port: &str, baud: u32) -> Result<Self> {
-        let device = makcu::Device::new(port, baud, Duration::from_millis(100));
-        device.connect()?;
-        let random = rand::rng();
-        Ok(Self { device, random })
+        if !ALLOWED_BAUD_RATE.contains(&baud) {
+            bail!("Baud rate out of range, allowed: {:?}", ALLOWED_BAUD_RATE);
+        }
+        let mut serial = serialport::new(port, DEFAULT_BAUD_RATE)
+            .timeout(Duration::from_millis(100))
+            .open()?;
+        if baud != DEFAULT_BAUD_RATE {
+            serial.write_all(&BAUD_CHANGE_COMMAND)?;
+            serial.flush()?;
+            serial.set_baud_rate(baud)?;
+            serial.clear(serialport::ClearBuffer::Input)?;
+            serial.clear(serialport::ClearBuffer::Output)?;
+            sleep(Duration::from_millis(100));
+            serial.write_all(VERIFY_COMMAND)?;
+            let mut verification_response = String::new();
+            let mut buffer = [0; 128];
+            loop {
+                match serial.read(&mut buffer) {
+                    Ok(bytes_read) => {
+                        if bytes_read > 0 {
+                            verification_response
+                                .push_str(&String::from_utf8_lossy(&buffer[..bytes_read]));
+                            if verification_response.contains("km.MAKCU") {
+                                break;
+                            }
+                        }
+                    }
+                    Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => {
+                        tracing::error!(
+                            "Verification MAKCU change baud rate timed out. Check the connection."
+                        );
+                        bail!(std::io::Error::new(
+                            std::io::ErrorKind::Other,
+                            "Timeout during verification"
+                        ));
+                    }
+                    Err(e) => {
+                        bail!(e);
+                    }
+                }
+            }
+        }
+        Ok(Self {
+            serial,
+            random: rand::rng(),
+        })
     }
 
-    pub fn move_shift(&self, dx: i32, dy: i32) -> Result<()> {
-        Ok(self.device.move_rel(dx, dy)?)
+    #[inline(always)]
+    fn cmd(&mut self, command: &str) -> Result<()> {
+        Ok(self
+            .serial
+            .write_all(format!("{}\r\n", command).as_bytes())?)
+    }
+
+    pub fn move_shift(&mut self, dx: i32, dy: i32) -> Result<()> {
+        self.cmd(format!("km.move({dx},{dy})").as_str())
     }
 
     pub fn move_bezier(&mut self, dx: i32, dy: i32) -> Result<()> {
@@ -35,10 +89,6 @@ impl MouseVirtual {
         };
         let ref_x = self.random.random_range(4..17);
         let ref_y = self.random.random_range(4..17);
-        Ok(self.device.move_bezier(dx, dy, steps, ref_x, ref_y)?)
-    }
-
-    pub fn close(&self) {
-        self.device.disconnect()
+        self.cmd(format!("km.move({dx},{dy},{steps},{ref_x},{ref_y})").as_str())
     }
 }

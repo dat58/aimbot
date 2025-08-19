9d155d4c50a854e33bdf02f79c734d1e7523835a
dat58 <vtdat58@gmail.com>
2025-08-19 08:47:59 +0000

    feat: Introduce stream module with NDI and UDP support

---
+++ src/stream/mod.rs
@@ -0,0 +1,55 @@
+mod ndi;
+mod udp;
+pub use ndi::*;
+pub use udp::*;
+
+use anyhow::Result;
+use crossbeam::queue::ArrayQueue;
+use opencv::core::Mat;
+use std::{
+    sync::Arc,
+    time::{Duration, Instant},
+};
+
+pub struct StreamInfo {
+    pub width: u32,
+    pub height: u32,
+    pub fps: u32,
+}
+
+pub trait StreamCapture: Send {
+    fn capture(&mut self) -> Result<Mat>;
+    fn stream_info(&self) -> Result<StreamInfo>;
+    fn reconnect(&mut self) -> Result<()>;
+}
+
+pub fn handle_capture(
+    mut cap: Box<dyn StreamCapture>,
+    queue: Arc<ArrayQueue<Mat>>,
+    retry_time: usize,
+    retry_interval: Duration,
+) {
+    loop {
+        let now = Instant::now();
+        if let Ok(mat) = cap.capture() {
+            tracing::debug!("[Stream] captured took: {:?}", now.elapsed());
+            queue.force_push(mat);
+        } else {
+            tracing::warn!("[Stream] unable to capture from the stream, try reconnecting...");
+            let mut reconnect_success = false;
+            for _ in 0..retry_time {
+                if cap.reconnect().is_ok() {
+                    reconnect_success = true;
+                    break;
+                }
+                std::thread::sleep(retry_interval);
+            }
+            if reconnect_success {
+                continue;
+            } else {
+                tracing::error!("[Stream] reconnect to the stream timed out, break the loop.");
+                break;
+            }
+        }
+    }
+}

98a7aee08bdfca06c5ef19935ee8dfd8bd2f049a
dat58 <vtdat58@gmail.com>
2025-08-21 19:13:46 +0000

    fix: Improve error handling and logging in stream capture loop
    This commit improves the error handling within the stream capture loop.
    Instead of just checking if `cap.capture()` returns `Ok`, it now uses a
    `match` statement to explicitly handle the `Err` case.
    
    When an error occurs during capture, it logs the error message and
    attempts to reconnect to the stream. This provides more detailed
    information about capture failures and enhances the robustness of the
    streaming process. Also, added `Debug` derive for `StreamInfo` struct.

--- src/stream/mod.rs
+++ src/stream/mod.rs
@@ -11,6 +11,7 @@ use std::{
     time::{Duration, Instant},
 };
 
+#[derive(Debug)]
 pub struct StreamInfo {
     pub width: u32,
     pub height: u32,
@@ -31,24 +32,27 @@ pub fn handle_capture(
 ) {
     loop {
         let now = Instant::now();
-        if let Ok(mat) = cap.capture() {
-            tracing::debug!("[Stream] captured took: {:?}", now.elapsed());
-            queue.force_push(mat);
-        } else {
-            tracing::warn!("[Stream] unable to capture from the stream, try reconnecting...");
-            let mut reconnect_success = false;
-            for _ in 0..retry_time {
-                if cap.reconnect().is_ok() {
-                    reconnect_success = true;
+        match cap.capture() {
+            Ok(mat) => {
+                tracing::debug!("[Stream] captured took: {:?}", now.elapsed());
+                queue.force_push(mat);
+            }
+            Err(e) => {
+                tracing::error!("[Stream] failed to capture next frame: {}, try reconnecting", e);
+                let mut reconnect_success = false;
+                for _ in 0..retry_time {
+                    if cap.reconnect().is_ok() {
+                        reconnect_success = true;
+                        break;
+                    }
+                    std::thread::sleep(retry_interval);
+                }
+                if reconnect_success {
+                    continue;
+                } else {
+                    tracing::error!("[Stream] reconnect to the stream timed out, break the loop.");
                     break;
                 }
-                std::thread::sleep(retry_interval);
-            }
-            if reconnect_success {
-                continue;
-            } else {
-                tracing::error!("[Stream] reconnect to the stream timed out, break the loop.");
-                break;
             }
         }
     }

d683f931b135b14859e6fb0c7c93fdd578e0b715
dat58 <vtdat58@gmail.com>
2025-10-23 02:54:24 +0000

    feat: Use EspButton struct to handle button press events

--- src/bin/button_test.rs
+++ src/bin/button_test.rs
@@ -1,37 +1,44 @@
-use std::time::Duration;
+use aimbot::esp_button::EspButton;
+use std::{
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+    },
+    time::Duration,
+};
+use tracing_subscriber::{EnvFilter, Layer, fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
 fn main() {
-    println!(
+    tracing_subscriber::registry()
+        .with(
+            fmt::Layer::new().with_writer(std::io::stdout).with_filter(
+                EnvFilter::try_from_default_env()
+                    .or_else(|_| EnvFilter::try_new("info"))
+                    .unwrap(),
+            ),
+        )
+        .init();
+    tracing::info!(
         "{:?}",
         serialport::available_ports().expect("No serial ports found!")
     );
     let port = std::env::var("port").expect("Please input an serial port");
-    let mut serial = serialport::new(&port, 115200)
-        .timeout(Duration::from_millis(150))
-        .open()
-        .unwrap();
-    let mut buf = [0u8; 8];
-    let mut last_state = false;
+    let state = Arc::new(AtomicBool::new(false));
+    let mut esp_button =
+        EspButton::new(&port, state.clone()).expect("Failed to connect to ESP button");
+    std::thread::spawn(move || {
+        esp_button.listen();
+    });
+    let mut last_state = state.load(Ordering::Acquire);
     loop {
-        match serial.read(&mut buf) {
-            Ok(count) if count > 0 => {
-                for i in 0..count {
-                    if buf[i] == 48 || buf[i] == 49 {
-                        if buf[i] == 49 {
-                            if last_state != true {
-                                println!("PRESSED");
-                                last_state = true;
-                            }
-                        } else {
-                            if last_state != false {
-                                println!("RELEASED");
-                                last_state = false;
-                            }
-                        }
-                    }
-                }
+        let current_state = state.load(Ordering::Acquire);
+        if last_state != current_state {
+            if current_state {
+                println!("PRESSED");
+            } else {
+                println!("RELEASED");
             }
-            _ => {}
+            last_state = current_state;
         }
         std::thread::sleep(Duration::from_millis(5));
     }

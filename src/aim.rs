56f8cbef126095be5702e91ca5bb4f34ecf30d6b
dat58 <vtdat58@gmail.com>
2026-09-06 08:47:05 +0000

    feat(aim): Add Horizon aim mode with random offset
    Introduces a new 'Horizon' aim mode to the aimbot.
    This mode incorporates random vertical offsets around the target,
    dynamically adjusting based on the crosshair's position relative
    to the bounding box to simulate human-like aiming.
    
    - Updated AIM_MODE_LENGTH to 5 to include the new mode.
    - Modified AimMode enum and related `From` and `Into` implementations.
    - Added `aim_horizon` function to calculate the aim point for this mode.
    - Integrated `rand::prelude::*` for random number generation.
    - Added Debug trait to AimMode.

--- src/aim.rs
+++ src/aim.rs
@@ -2,6 +2,7 @@ use crate::{
     config::{SCALE_ABDOMEN_Y, SCALE_CHEST_Y, SCALE_HEAD_Y, SCALE_NECK_Y},
     model::{Bboxes, Point2f},
 };
+use rand::prelude::*;
 use std::{
     fmt::Display,
     sync::{
@@ -10,9 +11,9 @@ use std::{
     },
 };
 
-pub const AIM_MODE_LENGTH: u8 = 4;
+pub const AIM_MODE_LENGTH: u8 = 5;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct AimMode(Arc<AtomicU8>);
 
 #[derive(Clone, Copy)]
@@ -21,6 +22,7 @@ pub enum Mode {
     Neck,
     Chest,
     Abdomen,
+    Horizon,
 }
 
 impl AimMode {
@@ -32,12 +34,18 @@ impl AimMode {
         self.0.store(mode.into(), Ordering::Release);
     }
 
-    pub fn aim(&self, bboxes: &Bboxes) -> Option<(Point2f, f32)> {
+    pub fn aim(
+        &self,
+        bboxes: &Bboxes,
+        crosshair: &Point2f,
+        random: &mut ThreadRng,
+    ) -> Option<(Point2f, f32)> {
         match self.mode() {
             Mode::Head => self.aim_head(bboxes),
             Mode::Neck => self.aim_neck(bboxes),
             Mode::Chest => self.aim_chest(bboxes),
             Mode::Abdomen => self.aim_abdomen(bboxes),
+            Mode::Horizon => self.aim_horizon(bboxes, crosshair, random),
         }
     }
 
@@ -100,6 +108,72 @@ impl AimMode {
             },
         }
     }
+
+    #[inline(always)]
+    pub fn aim_horizon(
+        &self,
+        bboxes: &Bboxes,
+        crosshair: &Point2f,
+        random: &mut ThreadRng,
+    ) -> Option<(Point2f, f32)> {
+        match bboxes.class_1.first() {
+            Some(bbox) => {
+                if crosshair.y() < bbox.ymin() {
+                    let delta_y = random.random_range(0..11.min(bbox.height() as i32)) as f32;
+                    let y_rand = crosshair.y() + delta_y;
+                    Some((
+                        Point2f::new((bbox.xmin() + bbox.xmax()) / 2., y_rand),
+                        bbox.width() / 2.,
+                    ))
+                } else if crosshair.y() > bbox.ymax() {
+                    let delta_y = random.random_range(0..11.min(bbox.height() as i32)) as f32;
+                    let y_rand = crosshair.y() - delta_y;
+                    Some((
+                        Point2f::new((bbox.xmin() + bbox.xmax()) / 2., y_rand),
+                        bbox.width() / 2.,
+                    ))
+                } else {
+                    Some((
+                        Point2f::new(
+                            (bbox.xmax() + bbox.xmin()) / 2.,
+                            bbox.ymax() - bbox.height() / 3.,
+                        ),
+                        (bbox.width() / 2.).max(bbox.height() / 2.),
+                    ))
+                }
+            }
+            _ => match bboxes.class_0.first() {
+                Some(bbox) => {
+                    if crosshair.y() < bbox.ymin() {
+                        let delta_y = random.random_range(0..21.min(bbox.height() as i32)) as f32;
+                        let y_rand = crosshair.y() + delta_y;
+                        Some((
+                            Point2f::new((bbox.xmin() + bbox.xmax()) / 2., y_rand),
+                            bbox.width() / 2.,
+                        ))
+                    } else if crosshair.y() > bbox.ymax() {
+                        let delta_y = random.random_range(0..21.min(bbox.height() as i32)) as f32;
+                        let y_rand = crosshair.y() - delta_y;
+                        Some((
+                            Point2f::new((bbox.xmin() + bbox.xmax()) / 2., y_rand),
+                            bbox.width() / 2.,
+                        ))
+                    } else {
+                        let delta_y = random.random_range(
+                            -((crosshair.y() - bbox.ymin()) as i32).min(10)
+                                ..((bbox.ymax() - crosshair.y()) as i32).min(12),
+                        ) as f32;
+                        let y_rand = crosshair.y() + delta_y;
+                        Some((
+                            Point2f::new((bbox.xmin() + bbox.xmax()) / 2., y_rand),
+                            bbox.width() / 2.,
+                        ))
+                    }
+                }
+                _ => None,
+            },
+        }
+    }
 }
 
 impl Default for AimMode {
@@ -115,6 +189,7 @@ impl From<Mode> for AimMode {
             Mode::Neck => AtomicU8::new(1),
             Mode::Chest => AtomicU8::new(2),
             Mode::Abdomen => AtomicU8::new(3),
+            Mode::Horizon => AtomicU8::new(4),
         }))
     }
 }
@@ -131,7 +206,8 @@ impl From<u8> for Mode {
             0 => Mode::Head,
             1 => Mode::Neck,
             2 => Mode::Chest,
-            _ => Mode::Abdomen,
+            3 => Mode::Abdomen,
+            _ => Mode::Horizon,
         }
     }
 }
@@ -143,6 +219,7 @@ impl Into<u8> for Mode {
             Mode::Neck => 1,
             Mode::Chest => 2,
             Mode::Abdomen => 3,
+            Mode::Horizon => 4,
         }
     }
 }
@@ -154,6 +231,7 @@ impl Display for AimMode {
             Mode::Neck => write!(f, "Neck"),
             Mode::Chest => write!(f, "Chest"),
             Mode::Abdomen => write!(f, "Abdomen"),
+            Mode::Horizon => write!(f, "Horizon"),
         }
     }
 }
